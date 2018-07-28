f99ea948774e426988f8b5462307b5523deb79c1
Manoj <40380004+ManojSilag@users.noreply.github.com>
2018-07-28 21:38:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("If you do like my website don't hesitate to subscribe. Your little info helps me to pile my data. Which would ultimately be used for my FINAL BE project.")
+alert("If you do like my website don't hesitate to subscribe. Your little info helps me to pile my data");
